886576b65fff5ce4fe990fb364405b4d7bcf2a95
kosenkomichael <kosenko_michael_samara@mail.com>
2025-10-14 16:07:52 +0400

    totalcost calculation

--- src/CarRentalService.Infrastructure/Repositories/RentalRepository.cs
+++ src/CarRentalService.Infrastructure/Repositories/RentalRepository.cs
@@ -10,6 +10,8 @@ public class RentalRepository : IRepository<Rental>
 {
     private readonly IMongoDatabase _database;
     private readonly IMongoCollection<Rental> _rentalColection;
+    private readonly IMongoCollection<Vehicle> _vehicleCollection;
+    private readonly IMongoCollection<ModelGeneration> _modelGenerationCollection;
 
     /// <summary>
     /// Initializes a new instance of the rental repository.
@@ -19,6 +21,8 @@ public class RentalRepository : IRepository<Rental>
     {
         _database = client.GetDatabase("car-rental");
         _rentalColection = _database.GetCollection<Rental>("rentals");
+        _vehicleCollection = _database.GetCollection<Vehicle>("vehicles");
+        _modelGenerationCollection = _database.GetCollection<ModelGeneration>("model-generations");
     }
 
     /// <inheritdoc/>
@@ -36,12 +40,30 @@ public class RentalRepository : IRepository<Rental>
     }
 
     /// <inheritdoc/>
-    public async Task<List<Rental>> ReadAllAsync() =>
-         await (await _rentalColection.FindAsync(Builders<Rental>.Filter.Empty)).ToListAsync();
+    public async Task<List<Rental>> ReadAllAsync()
+    {
+        var list = await (await _rentalColection.FindAsync(Builders<Rental>.Filter.Empty)).ToListAsync();
+        foreach (var rental in list)
+        {
+            var vehicle = await _vehicleCollection.Find(c => c.Id == rental.VehicleId).FirstOrDefaultAsync();
+            var modelGeneration = await _modelGenerationCollection.Find(c => c.Id == vehicle.ModelGenerationId).FirstOrDefaultAsync();
+
+            rental.TotalCost = (decimal)rental.RentalDurationHours * modelGeneration.RentalPricePerHour;
+        }
+        return list;
+    }
 
     /// <inheritdoc/>
-    public async Task<Rental?> ReadAsync(Guid id) =>
-        await _rentalColection.Find(c => c.Id == id).FirstOrDefaultAsync();
+    public async Task<Rental?> ReadAsync(Guid id)
+    {
+        var rental = await _rentalColection.Find(c => c.Id == id).FirstOrDefaultAsync();
+        var vehicle = await _vehicleCollection.Find(c => c.Id == rental.VehicleId).FirstOrDefaultAsync();
+        var modelGeneration = await _modelGenerationCollection.Find(c => c.Id == vehicle.ModelGenerationId).FirstOrDefaultAsync();
+
+        rental.TotalCost = (decimal)rental.RentalDurationHours * modelGeneration.RentalPricePerHour;
+
+        return rental;
+    }
 
     /// <inheritdoc/>
     public async Task<Rental?> UpdateAsync(Guid id, Rental entity)
