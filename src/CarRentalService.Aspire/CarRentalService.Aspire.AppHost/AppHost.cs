fb86c5113df7d1c4e0b4755629e34f591fb2d91b
kosenkomichael <kosenko_michael_samara@mail.com>
2025-10-18 13:53:23 +0400

    remove datavolume

--- src/CarRentalService.Aspire/CarRentalService.Aspire.AppHost/AppHost.cs
+++ src/CarRentalService.Aspire/CarRentalService.Aspire.AppHost/AppHost.cs
@@ -3,7 +3,6 @@ using Aspire.Hosting;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var mongoDb = builder.AddMongoDB("db")
-    //.WithDataVolume()
     .AddDatabase("car-rental");
 var api = builder.AddProject<Projects.CarRentalService_WebApplication>("api")
     .WithReference(mongoDb)
